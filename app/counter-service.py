f1d42edb8e7cec12fe369b9d7b4d421f3c7bc730
tom1187 <tomh1187@gmail.com>
2023-02-20 12:14:50 +0200

    another webhook test

--- app/counter-service.py
+++ app/counter-service.py
@@ -8,9 +8,7 @@ from fastapi import FastAPI
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        logging.StreamHandler()
-    ]
+    handlers=[logging.StreamHandler()]
 )
 
 app = FastAPI()
